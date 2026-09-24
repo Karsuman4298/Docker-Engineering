8bd2c329f60c2e1f014ae2173e9f4d5f6c2d6a5e
Karsuman4298 <karsuman4298@gmail.com>
2026-09-24 11:21:52 +0000

    pipeling working

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,66 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
+import click
 import pandas as pd
-from tqdm.auto import tqdm
 from sqlalchemy import create_engine
-import pandas as pd
-year=2021
-month=1
-
-pg_user="root"
-pg_pass="root"
-pg_host="localhost"
-pg_port="5432"
-pg_db="ny_taxi"
-chunksize=100000
-
-pd.__file__
-
-
-# Read a sample of the data
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-df = pd.read_csv(prefix + 'yellow_tripdata_2021-01.csv.gz')
-
-# Display first rows
-df.head()
-
-# Check data types
-df.dtypes
-
-# Check data shape
-df.shape
-
-
-# In[7]:
-
-
-df.head()
-
-
-# In[12]:
-
-
-x=df.shape[0]
-
-
-# In[13]:
-
-
-y=df["VendorID"].count()
-
-
-# In[17]:
-
-
-missing_vid=x-y
-missing_vid
-
-
-# In[18]:
-
+from tqdm.auto import tqdm
 
 dtype = {
     "VendorID": "Int64",
@@ -86,89 +30,48 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-df = pd.read_csv(
-    prefix + 'yellow_tripdata_2021-01.csv.gz',
-    nrows=100,
-    dtype=dtype,
-    parse_dates=parse_dates
-)
 
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
+    """Ingest NYC taxi data into PostgreSQL database."""
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-# In[19]:
+    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
-
-df.head()
-
-
-# In[20]:
-
-
-get_ipython().system('uv add sqlalchemy "psycopg[binary,pool]"')
-
-
-engine = create_engine(f'postgresql+psycopg://root:root@{pg_host}:{pg_port}/{pg_db}')
-
-
-# In[23]:
-
-
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
-
-
-# In[24]:
-
-
-df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
-
-
-# In[25]:
-
-
-df_iter = pd.read_csv(
-    prefix + 'yellow_tripdata_2021-01.csv.gz',
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator=True,
-    chunksize=100000
-)   
-
-
-# In[26]:
-
-
-get_ipython().system('uv add tqdm')
-
-
-# In[27]:
-
-
-
-first = True
-
-for df_chunk in tqdm(df_iter):
-
-    if first:
-        # Create table schema (no data)
-        df_chunk.head(0).to_sql(
-            name="yellow_taxi_data",
-            con=engine,
-            if_exists="replace"
-        )
-        first = False
-        print("Table created")
-
-    # Insert chunk
-    df_chunk.to_sql(
-        name="yellow_taxi_data",
-        con=engine,
-        if_exists="append"
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize,
     )
 
-    print("Inserted:", len(df_chunk))
+    first = True
 
+    for df_chunk in tqdm(df_iter):
+        if first:
+            df_chunk.head(0).to_sql(
+                name=target_table,
+                con=engine,
+                if_exists='replace'
+            )
+            first = False
 
-# In[ ]:
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists='append'
+        )
 
-
-
-
+if __name__ == '__main__':
+    run()
